5c35d49082861d6fdd0b41e995601f0bb1041096
KilianLamiral <kilian.lamiral@gmail.com>
2016-10-11 20:44:41 +0200

    AjoutClassTEST

---
+++ squeletteWeb/Métier/ClassTEST.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace squeletteWeb.Métier
+{
+    public class ClassTEST
+    {
+       
+
+        public string Test { get; set; }
+
+        public int TestID { get; set; }
+    }
+}
